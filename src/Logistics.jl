3d36cd51b9849daa9b3e8f3f657ad731bdb2dbdd
Yu-Chang YANG <yang.yc.allium@gmail.com>
2023-03-18 13:29:03 +0800

    docs: warn against conversion by `Logistic`

--- src/Logistics.jl
+++ src/Logistics.jl
@@ -19,6 +19,12 @@ export Logistic, logit, logistic, logisticate, complement, half
 
 A type representing real numbers in ``[0,1]`` (e.g. probabilities) by their 
 logit values.
+
+!!! warning
+	Do not use `Logistic` to convert number types! For example, 
+	`Logistic(0.39)` equals ``\\operatorname{logistic}(0.39) \\approx 0.60`` 
+	rather than ``0.39`` in value. To convert a number to an equivalent 
+	`Logistic` instance, use [`logisticate`](@ref) or `convert(Logistic, x)`.
 """
 struct Logistic{T<:AbstractFloat} <: Real
 	t::T
